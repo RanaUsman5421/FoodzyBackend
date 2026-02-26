1678aa04e7f05f5c1cb650ee9da99e40d129076d
RanaUsman5421 <153462482+RanaUsman5421@users.noreply.github.com>
2026-02-27 03:37:10 +0500

    Update sessions.js

--- config/sessions.js
+++ config/sessions.js
@@ -3,7 +3,6 @@ const { MongoStore } = require('connect-mongo');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-app.set('trust proxy', 1);
 
 module.exports = session({
   secret: process.env.SESSION_SECRET || 'default-secret-change-in-production',
