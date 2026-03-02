f07013427b896786fdaeea0ef5cda53043ba41d2
RanaUsman5421 <153462482+RanaUsman5421@users.noreply.github.com>
2026-03-02 14:30:59 +0500

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -2,6 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    secure: false,
     auth:{
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
